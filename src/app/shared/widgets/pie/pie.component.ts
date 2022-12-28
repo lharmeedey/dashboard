350aba54d8c4331e28f863dfdd3ab3db653bff69
Olamide <kolawolebasit@gmail.com>
2022-12-28 19:48:42 +0100

    Initial commit

---
+++ src/app/shared/widgets/pie/pie.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
+@Component({
+  selector: 'app-widget-pie',
+  templateUrl: './pie.component.html',
+  styleUrls: ['./pie.component.scss']
+})
+export class PieComponent implements OnInit {
+
+  
+
+
+  ngOnInit(): void {
+  }
+
+}
